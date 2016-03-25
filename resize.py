1f5c5a77632c52769281720f7b4cc1772c155a86
ryoogata <ryo.ogata@gmail.com>
2016-03-25 17:24:21 +0900

    bug fix
    正常にリサイズできるようになる

--- resize.py
+++ resize.py
@@ -3,25 +3,20 @@ import cv2
 import numpy as np
 import sys
 
-def half_size(im):
-        hight = im.shape[0]
-        width = im.shape[1]
-        half_size = cv2.resize(im,(hight/2,width/2))
-
-        cv2.imshow("half_size",half_size)
-        cv2.waitKey(0)
-
-        # ç”»åƒä¿å­˜
-        out_file_name = "after.jpg"
-        cv2.imwrite(out_file_name,half_size)
-
-        cv2.destroyAllWindows()  
-
 if __name__ == '__main__':
     param = sys.argv
 
+    # ‰æ‘œ‚Ì“Ç‚İ‚İ
     im = cv2.imread(param[1])
-    if not (im == None):
-        half_size(im)
-    else:
-        print 'Not exist'
+
+    # ‰æ‘œ‚ÌƒTƒCƒY‚ğæ“¾
+    hight = im.shape[0]
+    width = im.shape[1]
+
+    # ‰æ‘œƒTƒCƒY‚Ì•ÏX
+    half_size = cv2.resize(im,(width/2,hight/2))
+
+    # ‰æ‘œ‚Ì•\¦
+    cv2.imshow("Show Image", half_size)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
